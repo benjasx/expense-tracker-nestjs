ddc3acaed4d62d0e27f6f304569b979d572c9769
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-15 14:33:01 -0700

    Instalacion de TypeORM y configuracion de las variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CategoriasModule } from './categorias/categorias.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CategoriasModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CategoriasModule,
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.BD_NAME,
+      autoLoadEntities: true, // Carga automáticamente los archivos .entity.ts
+      synchronize: true, // Crea las tablas en Docker automáticamente al guardar
+    }),
+  ],
 })
 export class AppModule {}
