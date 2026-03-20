e3c5de2e40fffd112669db9fd6ae05238b175e21
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-20 09:54:38 -0700

    manejo de errores al crear un Usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { error } from 'console';
 
 @Injectable()
 export class AuthService {
@@ -15,6 +20,18 @@ export class AuthService {
     try {
       const user = this.userRepository.create(createUserDto);
       return await this.userRepository.save(user);
-    } catch (error) {}
+    } catch (error) {
+      this.handleDBError(error);
+    }
   }
+
+  private handleDBError = (error: any): never => {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs');
+  };
 }
