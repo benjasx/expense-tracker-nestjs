4b527a14790418a8b1c4366a6d4c8d1bbd943f76
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-21 06:26:22 -0700

    generar el JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,12 +10,16 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import * as bcrypt from 'bcrypt';
 import { LoginUserDto } from './dto';
+import { JwtPayload } from './interfaces/jwt.payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+
+    private readonly jwtService: JwtService,
   ) {}
 
   async create(createUserDto: CreateUserDto) {
@@ -30,6 +34,7 @@ export class AuthService {
       const { password: pass, ...userInfo } = user;
       return {
         ...userInfo,
+        token: this.getJwtToken({ email: user.email }),
       };
     } catch (error) {
       this.handleDBError(error);
@@ -51,8 +56,15 @@ export class AuthService {
     if (!bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid (password)');
 
-    return user;
-    //TODO: Rerornar JWT
+    return {
+      ...user,
+      token: this.getJwtToken({ email: user.email }),
+    };
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 
   private handleDBError = (error: any): never => {
