0c927665258dd1c9bf024f3da978203a83fe7d85
devbenjasx98 <benjasanchez175@gmail.com>
2026-03-23 10:54:50 -0700

    Auth en otros módulos

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -11,12 +11,15 @@ import {
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles.interface';
 
 @Controller('categorias')
 export class CategoriasController {
   constructor(private readonly categoriasService: CategoriasService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createCategoriaDto: CreateCategoriaDto) {
     return this.categoriasService.create(createCategoriaDto);
   }
--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -4,9 +4,10 @@ import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { Categoria } from '../categorias/entities/categoria.entity';
 import { Expense } from '../expenses/entities/expense.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Categoria, Expense])],
+  imports: [TypeOrmModule.forFeature([Categoria, Expense]), AuthModule],
   controllers: [SeedController],
   providers: [SeedService],
 })
